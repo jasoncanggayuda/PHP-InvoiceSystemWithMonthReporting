a69b4ae99772aa9035b3514f2bdf852f9d857551
jasoncanggayuda <jasoncanggayuda@gmail.com>
2023-01-04 09:44:16 +0700

    PHP code

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php
+/*******************************************************************************
+*  Invoice Management System                                                *
+*                                                                              *
+* Version: 1.0	                                                               *
+* Developer:  Abhishek Raj                                   				           *
+*******************************************************************************/
+
+include('header.php');
+include('functions.php');
+include_once("includes/config.php");
+
+?>
+
+
+
+<?php
+	include('footer.php');
+?>
